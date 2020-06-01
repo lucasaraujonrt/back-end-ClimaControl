0b42040d075ff44c15fa68b5ad29886c5dc4be33
LucasA <37451939+lucasaraujonrt@users.noreply.github.com>
2020-05-31 22:36:05 -0300

    Alter Column DB

--- back-end/src/controllers/createRoomController.js
+++ back-end/src/controllers/createRoomController.js
@@ -3,10 +3,11 @@ const connection = require("../database/connection");
 module.exports = {
   async index(req, res) {},
   async create(req, res) {
-    const { ideal_temperature, name, equipment } = req.body;
+    const { ideal_temperature, temperature, name, equipment } = req.body;
 
     await connection("room").insert({
       ideal_temperature,
+      temperature,
       name,
       equipment,
     });
--- back-end/src/database/migrations/20200531205819_create_room.js
+++ back-end/src/database/migrations/20200531205819_create_room.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("room", function (table) {
     table.increments();
     table.string("ideal_temperature").notNullable();
+    table.string("temperature").notNullable();
     table.string("name").notNullable();
     table.string("equipment").notNullable();
   });
